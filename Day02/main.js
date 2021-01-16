d403113569d0895c273a61b3f3a66d82d15df32e
Abdulsomod <shittu@codevillage.ng>
2021-01-16 16:54:24 +0100

    commit

---
+++ Day02/main.js
@@ -0,0 +1,11 @@
+// Exercise 1
+let challenge = '30 days of javascript';
+console.log(challenge)
+
+// Exercise 2
+console.log(challenge.length)
+
+// Exercise 3
+console.log(challenge.toUpperCase())
+
+// Exercise 4
