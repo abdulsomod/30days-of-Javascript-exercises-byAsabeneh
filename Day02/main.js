984f5aad1c59df378adcc10a62c0652eb493a79d
Abdulsomod <shittu@codevillage.ng>
2021-01-17 00:30:28 +0100

    commit

--- Day02/main.js
+++ Day02/main.js
@@ -1,11 +1,34 @@
-// Exercise 1
-let challenge = '30 days of javascript';
+// 1.
+let challenge = '30 Days of javascript';
+
+// 2.
 console.log(challenge)
 
-// Exercise 2
+// 3.
 console.log(challenge.length)
 
-// Exercise 3
+// 4.
 console.log(challenge.toUpperCase())
 
-// Exercise 4
+// 5.
+console.log(challenge.toLowerCase())
+
+// 6.
+console.log(challenge.substr(3,5))
+
+// 7.
+console.log(challenge.substr(3,20))
+
+// 8. 
+console.log(challenge.includes('script'))
+
+// 9.
+console.log(challenge.split())
+
+// 10. 
+console.log(challenge.split(' '))
+
+// 11.
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(string.split(','))
+
